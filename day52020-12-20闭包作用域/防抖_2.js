30797b94d5ea6885301267e29305074431bca858
caomengdie <2676171990@qq.com>
2021-07-23 23:22:59 +0800

    getName

---
+++ day52020-12-20闭包作用域/防抖_2.js
@@ -0,0 +1,26 @@
+function Person() {
+    this.getName = function a() {
+        console.log('2');
+    }
+}
+Person.getName = function a() {
+    console.log('3')
+}
+Person.prototype.getName = function () {
+    console.log('4')
+}
+var getName = function () {
+    console.log('5')
+}
+function getName() {
+    console.log('6')
+}//函数声明会提前所以会被覆盖
+Person.getName();//3
+// getName()//5
+// getName() 5
+new Person.getName//3
+new Person().getName()//2
+let p = new Person();
+console.log(p)
+p.getName()
+console.log(Person)
