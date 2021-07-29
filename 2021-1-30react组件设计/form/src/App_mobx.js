c04cc7789cb96ce7d96656cdf42f84f8e6952cfd
caomengdie <2676171990@qq.com>
2021-07-29 13:10:16 +0800

    mobx

---
+++ 2021-1-30react组件设计/form/src/App_mobx.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class Mobx extends React.Component {
+    render() {
+        return <div>mobx</div>
+    }
+}
+export default Mobx
--- 2021-1-30react组件设计/form/src/index.js
+++ 2021-1-30react组件设计/form/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Mobx from './App_mobx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Mobx />
   </React.StrictMode>,
   document.getElementById('root')
 );
