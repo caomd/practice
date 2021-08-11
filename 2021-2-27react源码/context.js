dabc882eeb4a1336b4e6ac3efe395dff695915b0
caomengdie <2676171990@qq.com>
2021-08-11 21:04:39 +0800

    context

---
+++ 2021-2-27react源码/context.js
@@ -0,0 +1,29 @@
+class Parent extends React.Component {
+    state = {
+        childContext: '123'
+    }
+    getChildContext{
+    return {
+        value: this.state.childContext
+    }
+}
+}
+
+class Child extends React.Component {
+    //想要在上层组件拿到哪些属性的声明，不然拿不到，这里没有的就获取不到
+    Child.contextTypes = {
+        value: PropTypes.string
+    }
+
+
+    //必须声明 
+    Parent.childContextType = {
+        value: ProtoTypes.string
+    }
+}
+
+export default () => {
+    <Parent>
+        <Child></Child>
+    </Parent>
+}
