d4a1c45eea82e335ab8f1f4fd18d6d323855e9c6
caomengdie <2676171990@qq.com>
2021-10-23 23:15:54 +0800

    interface

--- 2021-7-11-ts/book.ts
+++ 2021-7-11-ts/book.ts
@@ -3,16 +3,59 @@
 //     console.log(i)
 // }
 // for (let j of array) {
-//     console.log(j)
+//     console.log(j) 
 // }
-const fruits = new Set(['apple', 'pear', 'mango']);
-fruits['peach'] = 'Princess Peach! Make a wish';
-for (let fruit in fruits) {
-    console.log(fruit)//peach
+// const fruits = new Set(['apple', 'pear', 'mango']);
+// fruits['peach'] = 'Princess Peach! Make a wish';
+// for (let fruit in fruits) {
+//     console.log(fruit)//peach
+// }
+// for (let fruit of fruits) {
+//     console.log(fruit)
+//     // apple
+//     // pear
+//     // mango
+// }
+// function* generator() {
+//     const who = yield;
+//     console.log('hello ' + who)
+// }
+// const iterator = generator()
+// console.log(iterator.next());
+// // { value: undefined, done: false }
+
+// console.log(iterator.next('TypeScript'));
+
+// function* generatorPra() {
+//     const who = yield;
+//     console.log('hello ' + who);
+// }
+// const iteratorPra = generatorPra()
+// console.log('two', iteratorPra.next())
+// console.log('w', iteratorPra.next('TypeScript'))
+
+// function* generatorThrow() {
+//     try {
+//         yield 1;
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+// }
+// const iteratorThrow = generatorThrow()
+// console.log(iteratorThrow.next());
+// iteratorThrow.throw(new Error('something is wrong'))
+// console.log(iteratorThrow.next());
+
+// interface Teachers {
+//     teach(): void
+// };
+interface Student {
+    learn(): void
+};
+function getPerson(): Student {
+    return {} as Student
 }
-for (let fruit of fruits) {
-    console.log(fruit)
-    // apple
-    // pear
-    // mango
-}
+const person = getPerson();
+(<Student>person).learn();
+// (<Teacher>person).teach();
+
