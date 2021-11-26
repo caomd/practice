98d77200c10efa854e6f4dd3045af766f4c52a3f
caomengdie <2676171990@qq.com>
2021-11-26 18:54:57 +0800

    选择排序

--- javascript/数据结构和算法/10排序和搜素算法2021-11-25/选择排序.js
+++ javascript/数据结构和算法/10排序和搜素算法2021-11-25/选择排序.js
@@ -1,15 +1,46 @@
-//外循环控制迭代轮数 
-this.selectionSort = function () {
-    var length = array.length, indexMin
-    for (var i = 0; i < length; i++) {
-        indexMin = i
-        for (var j = i; j < length; j++) {
-            if (array[indexMin] > array[j]) {
-                indexMin = j
+var ArrayList = function () {
+    var array = [], length = 0
+    this.insert = function (element) {
+        array.push(element)
+        length++
+    }
+    //外循环控制迭代轮数 
+    this.selectionSort = function () {
+        var indexMin
+        for (var i = 0; i < length; i++) {
+            indexMin = i
+            for (var j = i; j < length; j++) {
+                if (array[indexMin] > array[j]) {
+                    indexMin = j
+                }
+            }
+            if (i !== indexMin) {
+                swap(array, i, indexMin)
             }
         }
-        if (i !== indexMin) {
-            swap(i, indexMin)
+    }
+    var swap = function (arr, index1, index2) {
+        //交换时使用中间值来存储某一交换项的值
+        var aux = arr[index1]
+        arr[index1] = arr[index2]
+        arr[index2] = aux
+    }
+    this.toString = function () {
+        var s = ''
+        for (var i = 0; i < array.length; i++) {
+            s += array[i] + '  '
         }
+        console.log(s)
     }
 }
+function createNonSortedArray(size) {
+    var array = new ArrayList()
+    for (var i = size; i > 0; i--) {
+        array.insert(i)
+    }
+    return array
+}
+var array = createNonSortedArray(5)
+array.toString()
+array.selectionSort()
+array.toString()
