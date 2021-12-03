1950370b811084f8677d50a2930370af3d0a7c09
caomengdie <2676171990@qq.com>
2021-12-03 09:59:55 +0800

    remove key

--- javascript/复习2021-12-3/q.md
+++ javascript/复习2021-12-3/q.md
@@ -2,7 +2,12 @@
  * @Author: caomd 
  * @Date: 2021-12-03 07:07:43 
  * @Last Modified by: caomd
- * @Last Modified time: 2021-12-03 07:10:00
+ * @Last Modified time: 2021-12-03 09:59:10
  */
 
-********二叉搜索树 
+********二叉搜索树****** 
+条件判断中 一个有return 那么其他的也要有return 不然会返回undefined
+删除一个节点总是失败 返回值赋值问题
+从左子树删除元素要node.left = removeNode(node.left,key)
+从右子树删除元素要node.right = removeNode(node.right,key)
+最后跳出所有条件判断要返回 node 
--- javascript/复习2021-12-3/二叉搜索树.js
+++ javascript/复习2021-12-3/二叉搜索树.js
@@ -2,7 +2,7 @@
  * @Author: caomd 
  * @Date: 2021-12-03 06:50:38 
  * @Last Modified by: caomd
- * @Last Modified time: 2021-12-03 07:09:10
+ * @Last Modified time: 2021-12-03 09:57:27
  */
 var BinarySearchTree = function () {
     var root = null, size = 0,
@@ -51,6 +51,92 @@ var BinarySearchTree = function () {
             inOrderTranversNode(node.right, cb, size)
         }
     }
+    //search a node
+    this.search = function (key) {
+        if (root) {
+            return searchNode(root, key)
+        }
+    }
+    var searchNode = function (node, key) {
+        if (node !== null) {
+            //比较和节点的大小在左子树还是右子树
+            if (node.key < key) {
+                //right
+                return searchNode(node.right, key)
+            } else if (node.key > key) {
+                return searchNode(node.left, key)
+            } else if (node.key === key) {
+                return node
+            }
+        } else {
+            return '未查找到搜索项'
+        }
+    }
+    this.max = function () {
+        if (root) {
+            return maxNodeFind(root)
+        }
+    }
+    var maxNodeFind = function (node) {
+        //大值均在右树上
+        var maxNode
+        if (node.right === null) {
+            return maxNode = node
+        }
+        return maxNodeFind(node.right)
+    }
+    this.min = function () {
+        if (root) {
+            return minNodeFind(root)
+        }
+    }
+    var minNodeFind = function (node) {
+        if (node.left === null) {
+            return node
+        }
+        return minNodeFind(node.left)
+    }
+    //删除节点
+    this.remove = function (key) {
+        if (root) {
+            root = removeNode(root, key)
+            return --size
+        }
+    }
+    var removeNode = function (node, key) {
+        if (node !== null) {
+            if (node.key > key) {
+                //left
+                node.left = removeNode(node.left, key)
+            } else if (node.key < key) {
+                node.right = removeNode(node.right, key)
+            } else if (node.key === key) {
+                //判断是否有子节点
+                if (node.left === null && node.right === null) {
+                    node = null
+                } else if (node.left === null) {
+                    node = node.right
+                } else if (node.right === null) {
+                    node = node.left
+                } else {
+                    //左右都有节点
+                    //取右树上最小值代替删除节点的值 保证左边节点小于根 右节点大于根
+                    var minRightNode = findMinRight(node.right)
+                    console.log('右子树最小值为：', minRightNode)
+                    node.key = minRightNode.key
+                    //删除右子树上的最小值的节点
+                    node.right = removeNode(node.right, minRightNode.key)
+                }
+            }
+            return node
+        }
+    }
+    var findMinRight = function (node) {
+        if (node.left !== null) {
+            return findMinRight(node.left)
+        }
+        return node
+    }
 }
 var print = (
     function () {
@@ -58,7 +144,7 @@ var print = (
         return function (val, size) {
             cache.push(val)
             if (cache.length === size) {
-                console.log(cache.join('   '))
+                console.log('中序遍历：', cache.join('  '))
                 cache = []
             }
         }
@@ -71,4 +157,9 @@ tree.insert(70);
 tree.insert(40);
 tree.insert(10);
 tree.insert(35);
+tree.inOrderTranvers(print)
+console.log('查找到搜索项结果是：', tree.search(35))
+console.log('最大节点为：', tree.max())
+console.log('最小节点为：', tree.min())
+console.log('删除元素：30,返回size', tree.remove(30))
 tree.inOrderTranvers(print)
