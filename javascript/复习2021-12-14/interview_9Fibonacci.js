683e3702e87c9ce69bcb7d1b18988c8f9f69400b
caomengdie <2676171990@qq.com>
2021-12-14 18:23:42 +0800

    interview_9

---
+++ javascript/复习2021-12-14/interview_9Fibonacci.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: caomd
+ * @Date: 2021-12-14 18:14:23
+ * @Last Modified by: caomd
+ * @Last Modified time: 2021-12-14 18:22:50
+ */
+//fibonacci recursion actually tree traverse 
+var Fibonacci = function (n) {
+    var n0 = 0, n1 = 1, sum = 0
+    for (var i = 2; i <= n; i++) {
+        sum = n0 + n1
+        n0 = n1//n0 -> next
+        n1 = sum//n1->next
+    }
+    return sum
+}
+console.log(Fibonacci(6))
