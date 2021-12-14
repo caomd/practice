e106bd43f9ccefb7c6998371a9cff3f1d00b9750
caomengdie <2676171990@qq.com>
2021-12-14 09:41:38 +0800

    reverseBetween

--- javascript/复习2021-12-13/q.md
+++ javascript/复习2021-12-13/q.md
@@ -2,7 +2,7 @@
  * @Author: caomd 
  * @Date: 2021-12-13 17:20:15 
  * @Last Modified by: caomd
- * @Last Modified time: 2021-12-13 17:22:04
+ * @Last Modified time: 2021-12-14 08:52:43
  */
 
 ************interview_19
@@ -10,3 +10,12 @@ not recursion complete
 *************interview_20
 use case only one column or one row or one number these situation
 and think about column and row and startIndex relations
+
+**************reverseList
+successor is global variables when in reverseN function is closure
+first find base case and then
+find the new head
+var last = recursionFn()
+last is the first one
+and then head.next.next = head  switch position 
+head.next = null break joint or head.next = successor 
---
+++ javascript/复习2021-12-13/reverseList.js
@@ -0,0 +1,75 @@
+/*
+ * @Author: caomd 
+ * @Date: 2021-12-13 22:12:09 
+ * @Last Modified by: caomd
+ * @Last Modified time: 2021-12-14 08:31:26
+ */
+var LinkedList = function () {
+    var Node = function (key) {
+        this.key = key
+        this.next = null
+    }
+    this.head = null
+    this.size = 0
+    this.append = function (key) {
+        var node = new Node(key)
+        if (this.head === null) {
+            this.head = node
+        } else {
+            var current = this.head
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node
+        }
+        return this.size++
+    }
+    this.print = function () {
+        var s = ''
+        if (this.head) {
+            var current = this.head
+            while (current.next) {
+                s += current.key + '  '
+                current = current.next
+            }
+            s += current.key
+        }
+        console.log(s)
+    }
+}
+var reverse = function (head) {
+    //base case
+    if (head === null || head.next === null) {
+        return head
+    }
+    //new head node
+    var last = reverse(head.next)
+    //change position
+    head.next.next = head
+    //break joint
+    head.next = null
+    return last
+}
+var successor = null
+var reverseN = function (head, n) {
+    //next node
+    if (n === 1) {
+        successor = head.next
+        return head
+    }
+    var last = reverseN(head.next, n - 1)
+    head.next.next = head
+    head.next = successor
+    return last
+}
+var linked = new LinkedList()
+linked.append(1)
+linked.append(2)
+linked.append(3)
+linked.append(4)
+linked.append(5)
+linked.append(6)
+linked.print()
+// var ret = reverse(linked.head)
+// console.log(ret)
+reverseN(linked.head, 3)
