49201233e235759ea0336b88fe9d91a0abe3179b
caomengdie <2676171990@qq.com>
2021-12-23 20:28:07 +0800

    openLock

---
+++ javascript/review2021-12-23/openLock.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: caomd 
+ * @Date: 2021-12-23 19:12:16 
+ * @Last Modified by: caomd
+ * @Last Modified time: 2021-12-23 20:25:46
+ */
+var openLock = function (deadStr, target) {
+    var visited = [], start = '0000',
+        deadArr = deadStr.split('')
+    var visited[start] = true
+    for (var )
+}
+var upWay = function (n) {
+    var str = ''
+    if (num === '9') return str = '0'
+    else {
+        var num = n * 1
+        return num++
+    }
+}
+var downWay = function (n) {
+    var str = ''
+    if (num === '0') return str = '9'
+    else {
+        var num = n * 1
+        return num--
+    }
+}
