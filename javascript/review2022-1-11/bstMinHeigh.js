5df5ef1cb81cbb1775fe5b6446e4ea655583d96b
caomengdie <2676171990@qq.com>
2022-01-10 20:58:46 +0800

    1-10

---
+++ javascript/review2022-1-11/bstMinHeigh.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: caomd 
+ * @Date: 2022-01-10 20:57:19 
+ * @Last Modified by: caomd
+ * @Last Modified time: 2022-01-10 20:57:39
+ */
+var bst = function () {
+    var Node
+}
+var bstMinHeight = function (root) {
+
+}
